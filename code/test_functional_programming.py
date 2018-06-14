47843045814da50a7019cc35622958b60dc9222b
朱磊 <zhulei199010@yahoo.com>
2018-06-14 22:10:00 +0800

    .

--- code/test_functional_programming.py
+++ code/test_functional_programming.py
@@ -60,3 +60,25 @@ if abs(str2float('123.456') - 123.456) < 0.00001:
     print('测试成功!')
 else:
     print('测试失败!')
+
+
+# 利用闭包返回一个计数器函数，每次调用它返回递增整数
+def createCounter():
+    n = 0
+
+    def counter():
+        nonlocal n
+        n = n + 1
+        return n
+
+    return counter
+
+
+# 测试:
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA())  # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
